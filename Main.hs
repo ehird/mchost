6c74c7208222f08aae7cfabb59dbca8b569f2881
Elliott Hird <penguinofthegods@gmail.com>
2011-09-18 23:17:06 +0100

    Make the main program print out packets from stdin
    This is useful for testing the ClientPacket parser; a non-trivial
    355 KiB dump of packets sent by the official client parses perfectly.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
 import MC.Protocol
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.Serialize (Get)
+import qualified Data.Serialize as SE
+import System.IO
+import System.Exit
+
+printDump :: ByteString -> IO ()
+printDump dump
+  | B.null dump = return ()
+  | otherwise =
+    case SE.runGetState (SE.get :: Get ClientPacket) dump 0 of
+      Left err -> hPutStr stderr err >> exitFailure
+      Right (p, dump') -> print p >> printDump dump'
 
 main :: IO ()
-main = print (CKeepAlive 42)
+main = B.getContents >>= printDump
