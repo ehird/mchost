d6095c0f54cb32440466826017f7365544604c64
Elliott Hird <penguinofthegods@gmail.com>
2011-09-19 23:56:21 +0100

    Make the test program support server packet dumps
    This concludes the 22 commits that constitute a cleaner, faster,
    revisionist history of the development of the server packet support.
    It's a lie, but a much more pleasant lie than the one huge commit and
    four small ones that this history replaces.

--- Main.hs
+++ Main.hs
@@ -6,15 +6,22 @@ import qualified Data.ByteString as B
 import Data.Serialize (Get)
 import qualified Data.Serialize as SE
 import System.IO
+import System.Environment
 import System.Exit
 
-printDump :: ByteString -> IO ()
-printDump dump
+printDump :: (Show a) => Get a -> ByteString -> IO ()
+printDump get dump
   | B.null dump = return ()
   | otherwise =
-    case SE.runGetState (SE.get :: Get ClientPacket) dump 0 of
+    case SE.runGetState get dump 0 of
       Left err -> hPutStr stderr err >> exitFailure
-      Right (p, dump') -> print p >> printDump dump'
+      Right (p, dump') -> print p >> printDump get dump'
 
 main :: IO ()
-main = B.getContents >>= printDump
+main = do
+  [origin] <- getArgs
+  dump <- B.getContents
+  case origin of
+    "client" -> printDump (SE.get :: Get ClientPacket) dump
+    "server" -> printDump (SE.get :: Get ServerPacket) dump
+    _ -> hPutStrLn stderr "usage: mchost {client,server} < dump" >> exitFailure
