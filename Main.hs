6c8a4f1ad7c97bad27c5cb0dfaf1a178361a5900
Elliott Hird <penguinofthegods@gmail.com>
2011-09-22 20:43:36 +0100

    Initial networking/iterIO code: a kick-only server
    I know that packet serialisation is slightly broken somehow, since
    passing the result of a packet read to a write produces a different,
    wrongly-sized file for my server packet dumps, although not the few
    client dumps I tried. But this works, so I figure I might as well
    commit it, even if it does print a broken pipe error for every
    connection received.

--- Main.hs
+++ Main.hs
@@ -1,27 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import MC.Protocol
 import Data.ByteString (ByteString)
-import qualified Data.ByteString as B
-import Data.Serialize (Get)
+import Data.Serialize (Get, Putter)
 import qualified Data.Serialize as SE
+import qualified Data.Text as T
+import Data.IterIO
+import qualified Data.IterIO.Iter as Iter
+import Control.Applicative
 import System.IO
 import System.Environment
 import System.Exit
+import Control.Concurrent
+import Network
 
-printDump :: (Show a) => Get a -> ByteString -> IO ()
-printDump get dump
-  | B.null dump = return ()
-  | otherwise =
-    case SE.runGetState get dump 0 of
-      Left err -> hPutStr stderr err >> exitFailure
-      Right (p, dump') -> print p >> printDump get dump'
+getI :: (Monad m, Show t) => Get t -> Inum ByteString [t] m a
+getI m = mkInum $ loop (SE.runGetPartial m)
+  where loop continue = do
+          str <- dataI
+          case continue str of
+            SE.Fail err -> throwParseI err
+            SE.Partial k -> loop k
+            SE.Done a s -> ungetI s >> return [a]
+
+enumPut :: (Monad m, Show t) => Putter t -> Inum [t] ByteString m a
+enumPut f = mkInum (SE.runPut . mapM_ f <$> dataI)
+
+handle :: HostName -> PortNumber -> Inum [ClientPacket] [ServerPacket] IO ()
+handle clientHost clientPort = mkInum $ do
+  return [SKick $ "ollies outie " `T.append` T.pack (show (clientHost,clientPort))]
+
+serverLoop :: Socket -> IO ()
+serverLoop server = loop
+  where loop = do
+          (client, clientHost, clientPort) <- accept server
+          hSetBuffering client NoBuffering
+          (clientI, enumClient) <- iterHandle client
+          putStrLn $ "Handling connection from " ++ show (clientHost,clientPort)
+          _ <- forkIO . Iter.run
+             $ enumClient
+            .| getI (SE.get :: Get ClientPacket)
+            .| handle clientHost clientPort
+            .| enumPut (SE.put :: Putter ServerPacket)
+            .| clientI
+          loop
 
 main :: IO ()
-main = do
-  [origin] <- getArgs
-  dump <- B.getContents
-  case origin of
-    "client" -> printDump (SE.get :: Get ClientPacket) dump
-    "server" -> printDump (SE.get :: Get ServerPacket) dump
-    _ -> hPutStrLn stderr "usage: mchost {client,server} < dump" >> exitFailure
+main = withSocketsDo $ do
+  args <- getArgs
+  port <- case map reads args of
+    [[(n,"")]] -> return $ fromIntegral (n :: Int)
+    _ -> hPutStrLn stderr "usage: mchost <port>" >> exitFailure
+  server <- listenOn (PortNumber port)
+  putStrLn $ "Listening on port " ++ show port ++ "..."
+  serverLoop server
